7493fb59a980be4a6a0433b4e3d7b7cd8efcecf7
Braihan <braihanleon@gmail.com>
2022-06-22 22:53:30 -0300

    cambio

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 
 window.alert("Bienvenido con esta App usted puede cotizar el Dolar/Pesos");
 
-const COTIZACION_DOLAR = 150;
+const COTIZACION_DOLAR = 220;
 const cotizarDolar = (pesos) => pesos / COTIZACION_DOLAR;
 const cotizarPesos = (dolar) => dolar * COTIZACION_DOLAR;
 let seleccion = prompt("SELECCIONAR COTIZACION \n 1 - DOLARES A PESOS \n 2 -  PESOS A DOLAR ");
